041e95b93f9ee85a61aacb33012668957e9e4610
SadisInc <62671451+SadisInc@users.noreply.github.com>
2022-08-21 17:54:31 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  //task 1.1
+  
+int data = 255;
+float payload = 6.153;
+bool flag = true;
+char ch = '#';
+cout << data << endl;
+cout << payload << endl;
+cout << flag << endl;
+cout << ch << endl;
+cout << endl;
+  
+  // task 1.2
+  
+cout << "data value is : " << data << endl;
+cout << "payload value is : " << payload <<endl;
+cout << "flag value is : " << flag << endl;
+cout << "ch value is : " << ch << endl;
+cout << endl;
+  
+  //task 2
+  
+int x1 = 10;
+cout << "x1 = " << x1 << endl;
+int x2 = x1 + 100;
+cout << "x2 = x1 + 100 = " << x2 << endl;
+int x3 = x2 - 20;
+cout << "x2 - 20 = " << x3 << endl;
+int x4 = x3 * 3;
+cout << "x3 * 3 = " << x4 << endl;
+int x5 = x4 / 2;
+cout << "x3 / 2 = " << x5 << endl;
+}
