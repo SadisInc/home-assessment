9156a472168fa6fd3a0363d3be9869ea4198593b
SadisInc <62671451+SadisInc@users.noreply.github.com>
2022-08-23 14:04:15 +0300

    Add files via upload

---
+++ task1 (remake).cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ {
+   /*             task 1.1          */ 
+  
+int data = 255;
+float payload = 6.153;
+bool flag = true;
+char ch = '#';
+cout << data << endl;
+cout << payload << endl;
+cout << flag << endl;
+cout << ch << endl;
+cout << endl;
+  
+  /*             task 1.2           */ 
+  
+cout << "data value is : " << data << endl;
+cout << "payload value is : " << payload <<endl;
+cout << "flag value is : " << flag << endl;
+cout << "ch value is : " << ch << endl;
+cout << endl;
+  
+  /*             task 2            */ 
+  
+int x1 = 10;
+cout << "x1 = " << x1 << endl;
+int x2 = x1 + 100;
+cout << "x2 = x1 + 100 = " << x2 << endl;
+int x3 = x2 - 20;
+cout << "x2 - 20 = " << x3 << endl;
+int x4 = x3 * 3;
+cout << "x3 * 3 = " << x4 << endl;
+int x5 = x4 / 2;
+cout << "x3 / 2 = " << x5 << endl;
+  
+  /*             task 3            */ 
+
+  float g = 0;  // float uses only for fractional numbers
+  float h = 1;  // float uses only for fractional numbers
+  double = 1.5426; // we haven`t variable
+
+  /*             task 4            */
+
+ int value = 3;
+ value = value + 1;
+ cout << Value << endl; // here we have a capital letter "V"
+   
+  /*             task 5            */
+   int userID = 0x12; // "x" it is not a multiplication operation
+   userID += 5;
+   Cout << userID << Endl; // here we have a syntax error "C" , "E" 
+   }
