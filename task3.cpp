a96734bd601e280e48fc6a83277387ea465a91b9
SadisInc <62671451+SadisInc@users.noreply.github.com>
2022-09-25 12:12:22 +0300

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+  // 1. Loops (for/while/do while)
+    // 1.1.0 Write a program that prints your name 8 times.
+    // 1.1.2 Write a program that prints integer numbers in the range 0 to 100.
+    // 1.1.3 Write a program that prints integer numbers in the range -10 to 10.
+    // 1.1.4 Write a program that prints integer numbers in the range -100 to 100.
+   
+   // 1.1.0
+//    int x = 0;
+//    for (size_t i = 0; i < 8; i++)
+//    {
+//     if (x == i)
+//     {
+//       cout << "Yuriy" << endl;  
+//       x+= 1;
+//     }
+    
+//    }
+  
+
+
+   //1.1.2
+  
+//   for (size_t i = 0; i <= 100; i++)
+//   {
+//    cout << i << endl;
+//   }
+ 
+
+
+
+  //1.1.3
+
+
+//    int i = -10;
+//   do
+//   {
+//    cout << i << endl;
+//    i++;
+//   } while (i <= 10);
+
+  
+  //1.1.4
+   
+
+//    int i = -100;
+//    do
+//    {
+//     cout << i << endl;
+//     i++;
+
+//    } while (i <= 100);
+
+
+
+
+
+
+
+  //2.1 Write a program that accepts a positive integer N from the user. 
+    // Make sure that the user enters exactly a positive number. 
+    // After that - sum up a series of numbers from 0 to the entered number N (0 + 1 + 2 + … + N). 
+    // For example, if user enter 3 (N = 3), program must print your 6, what means 1+2+3. 
+    // You must use for loop.
+
+
+   
+  //  int N = 0;
+  //  int y = 0;
+  //  cout << "Enter number : ";
+  //  cin >> N;
+  //  for (int i = 0; i <= N; i++)
+  //  {
+  //    y = y + i;
+    
+  //  }
+  //  cout << y << endl;
+    
+    
+    
+    // 3*. A timer is a software program or hardware device in your PC, smartphone, laptop that keeps track of the elapsed time between two events. These events could be mouse clicking, opening new browser window, start Minecraft and CS:GO and so on. Timer count time in “ticks”. A number of ticks could be a pretty large number, to represent 1 second.
+    // 	Let’s say that 1 second = 10 000 ticks. Simulate that you have a time span of 20 seconds, where:
+    //         - every 10 seconds the “My-Homework-N4.docx” auto save process occurs;
+    //         - every 5 seconds a browser page is refreshed;
+    //         - every 1 seconds it polls for the state Wi-Fi connection;
+    //         - every 1000 ticks it polls for the state of the connected charger;
+    //         - every 100 ticks it polls for the state of the left mouse button.
+    // At given intervals, you just need to PRINT THE NAME OF THE PROCESS, for example: “Browser page has refreshed!”, and so on.
+      
+  
+    
+
+   
+
+ 
+
+
+
+   
+
+
+
+
+  return 0;
+}
